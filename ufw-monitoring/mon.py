c2413892ce4062b994405a6896efc8f3618c2652
Jakub Partyka <kontakt@jakub-partyka.pl>
2021-08-04 12:49:51 +0200

    Update mon.py

--- ufw-monitoring/mon.py
+++ ufw-monitoring/mon.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python2.7
 
 # export number of connections blocked by UFW as a prometheus metric
-# v.0.2
+# v.0.3
 
 from datetime import date
 from prometheus_client import start_http_server, Gauge
 import re
 import time
+
+# config
+delay=10    # delay between measurments
+port=50028  # metrics expose port
 
 # date 
 t = date.today().strftime("%b %e.*")
@@ -30,12 +34,11 @@ def monitor():
     f.close()
     g.set(counter)
 
-    time.sleep(20)
-
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
-    start_http_server(8080)
+    start_http_server(port)
     # Generate some requests.
     while True:
         monitor()
+        time.sleep(delay)
